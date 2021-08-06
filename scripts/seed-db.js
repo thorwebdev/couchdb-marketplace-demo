406a413da6004de41b6ace622d5208770d34cf23
Thorsten Schaeff <thorsten.schaeff@gmail.com>
2021-08-06 17:35:57 +0800

    Script to seed DB with some data.

---
+++ scripts/seed-db.js
@@ -0,0 +1,34 @@
+const nano = require('nano')('http://admin:password@localhost:5984');
+
+const seedDB = async () => {
+  try {
+    try {
+      await nano.db.destroy('products');
+    } catch (error) {
+      console.log(error.description);
+    }
+    await nano.db.create('products');
+    const products = nano.use('products');
+    const response = await products.insert({
+      seller: 'admin',
+      name: 'Banana',
+      description: 'A curvy yellow fruit.',
+      images: [
+        'https://image.shutterstock.com/image-photo/bananas-grapes-600w-518328943.jpg',
+      ],
+      price: {
+        amount: 299,
+        currency: 'sgd',
+      },
+    });
+    // succeeded
+    console.log(response);
+    // Create users table
+    await nano.db.create('_users');
+  } catch (e) {
+    // failed
+    console.error(e);
+  }
+};
+
+seedDB();

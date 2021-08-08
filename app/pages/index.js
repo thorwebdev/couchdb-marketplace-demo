1d900b79a0641aeb3bd136f47ba76d5a8358eb03
Thorsten Schaeff <thorsten.schaeff@gmail.com>
2021-08-08 22:07:40 +0800

    Add responsive layout

--- app/pages/index.js
+++ app/pages/index.js
@@ -47,12 +47,11 @@ export default function Home({ products }) {
       <footer className="flex items-center justify-center w-full h-24 border-t">
         <a
           className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://thor.news"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          Made by thorwebdev ⚡️
         </a>
       </footer>
     </div>
